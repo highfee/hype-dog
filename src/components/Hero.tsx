7ed99d06cd93a37399d42f8fb6845f830c7a264e
highfee <odediranifeoluwa7@gmail.com>
2024-05-09 00:01:48 +0100

    ife: hero done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import ContainerLayout from "./ContainerLayout";
 
 import { Sedan } from "next/font/google";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const sedan = Sedan({
   subsets: ["latin"],
@@ -31,14 +32,29 @@ const Hero = () => {
               Have fun with HypeDog ($HYPD) <br /> The New Meme Coin Boss of The
               Base Network and get $HYPD to be part of our trip to the moon
             </p>
+
+            <div className="mt-20 flex items-center gap-10 text-lg">
+              <Link
+                href=""
+                className="py-2 px-8 border border-white rounded-md hover:scale-105 "
+              >
+                Buy Hype Dog
+              </Link>
+              <Link
+                href=""
+                className="py-2 px-8 bg-[#0f1907] border border-[#0f1907] rounded-md hover:scale-105"
+              >
+                View on Explorer
+              </Link>
+            </div>
           </div>
           <div className="flex-[2]">
             <Image
               src="/image1-transparent.png"
               alt=""
               height="400"
-              width="400"
-              className=""
+              width="500"
+              className="object-fit"
             />
           </div>
         </div>
